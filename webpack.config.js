838e76bb3db864f80bf5e66e143da1e2d9c1d6bb
Arti Parmar <153267233+arti1109@users.noreply.github.com>
2026-04-03 14:05:42 +0530

    Updated entry point and added HtmlWebpackPlugin for production build

--- webpack.config.js
+++ webpack.config.js
@@ -2,52 +2,31 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/main.js',  // Changed entry point
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true,
     },
     module: {
         rules: [
             {
-                test: /\.(png|jpg|jpeg|gif|svg|glsl)$/, 
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[path][name].[ext]',
-                        outputPath: 'assets/',
-                    },
-                }],
-            },
-            {
-                test: /\.js$/, 
-                exclude: /node_modules/, 
+                test: /\.js$/,
+                exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
                 },
             },
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './index.html',
+            template: './src/index.html',
             filename: 'index.html',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeAttributeQuotes: true,
+            },
         }),
     ],
-    resolve: {
-        alias: {
-            three: path.resolve(__dirname, 'node_modules/three'),
-        },
-    },
-    devtool: 'source-map',
-    mode: 'production',
-    performance: {
-        hints: 'warning',
-        maxEntrypointSize: 512000,
-        maxAssetSize: 512000,
-    },
 };
